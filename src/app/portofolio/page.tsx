5248717cc0ab3b3c859f5042eeb2bb0cf389cafb
ilham Suryana <szuryanailham090102@gmail.com>
2025-06-17 10:32:45 +0700

    adding image

--- src/app/portofolio/page.tsx
+++ src/app/portofolio/page.tsx
@@ -4,10 +4,10 @@ import React, { useState } from "react";
 import Image from "next/image";
 
 const imageGroups = [
-  ["https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg", "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg", "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg"],
-  ["https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg", "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg", "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg"],
-  ["https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg", "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg", "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg"],
-  ["https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg", "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg", "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg"],
+  ["/images/wisuda/photo1.jpg", "/images/wisuda/photo2.jpg", "/images/wisuda/photo3.jpg"],
+  ["/images/wisuda/photo4.jpg", "/images/wisuda/photo5.jpg", "/images/wisuda/photo6.jpg"],
+  ["/images/yearbook/photo1.jpg", "/images/yearbook/photo2.jpg", "/images/yearbook/photo3.jpg"],
+  ["/images/yearbook/photo4.jpg", "/images/yearbook/photo5.jpg", "/images/yearbook/photo6.jpg"],
 ];
 
 export default function Page() {
